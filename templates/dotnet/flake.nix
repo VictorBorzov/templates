2e5824400b818453eef7730e8d434f4a154df61f
Victor Borzov <borzov.vk@protonmail.com>
2024-06-09 15:14:56 +0200

    feat: Dotnet self-contained build

--- templates/dotnet/flake.nix
+++ templates/dotnet/flake.nix
@@ -1,25 +1,26 @@
 {
   description = "Dotnet app build";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "nixpkgs/nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-   {
-    packages.x86_64-linux.default = pkgs.buildDotnetModule {
-      pname = "HelloWorld";
-      version = "0.1";
-      src = ./.;
-      projectFile = "./HelloWorld.csproj";
-      nugetDeps = ./deps.nix;
-      dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0_1xx;
-      dotnet-runtime = pkgs.dotnetCorePackages.dotnet_8.runtime;
-      executables = [ "HelloWorld" ];
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      packages.x86_64-linux.default = pkgs.buildDotnetModule {
+        pname = "HelloWorld";
+        version = "0.1";
+        src = ./.;
+        projectFile = "./HelloWorld.csproj";
+        nugetDeps = ./deps.nix;
+        dotnet-sdk = pkgs.dotnetCorePackages.dotnet_8.sdk;
+        selfContainedBuild = true;
+        dotnetFlags = [
+          "-p:InvariantGlobalization=true"
+          "-p:PublishTrimmed=true"
+          "-p:PublishSingleFile=true"
+        ];
+        executables = [ "HelloWorld" ];
+      };
     };
-  };
 }
 
